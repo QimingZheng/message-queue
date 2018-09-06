69c901b19841c187e7a01a1a97a9bee140bb3e49
QimingZheng <zqmmqz@sjtu.edu.cn>
2018-09-06 11:16:33 +0800

    first optimization

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cstring>
+#include <thread>
+#include "queue_store.h"
+
+using namespace std;
+using namespace race2018;
+
+#define THREAD_NUM 1000
+#define MSG_NUM 100
+#define QUE_NUM 20
+
+void put_get(int tid, queue_store &store){
+    
+    const string quename = "Queue-" + to_string(tid%QUE_NUM);
+    for (int i = 0; i < MSG_NUM; i++) {
+        string slogan = string("abcdefghijklmnopqrst+abcdefghijklmnopqrst+abcdefghijklm") + to_string(i);
+        char* data = new char[slogan.size() + 1];
+        strcpy(data, slogan.c_str());
+        MemBlock msg = {static_cast<void*>(data),
+                        static_cast<size_t>(slogan.size())
+        };
+        store.put(quename, msg);
+    }
+
+     vector<MemBlock> list = store.get(quename, 10, 500);
+    /*
+    for (MemBlock &item : list) {
+        char* msg = static_cast<char *>(item.ptr);
+        cout << msg << endl;
+    }
+    */
+    cout<< "Thread " << tid << " Done!\n";
+}
+
+
+int main(int argc, char* argv[]) {
+
+    queue_store store;
+
+    std::thread t[THREAD_NUM];
+    for (int i=0;i<THREAD_NUM;i++)
+        t[i] = std::thread(put_get, i, std::ref(store));
+    
+    for (int i=0;i<THREAD_NUM;i++)
+        t[i].join();
+    return 0;
+}
---
+++ queue_store.cpp
@@ -0,0 +1,51 @@
+#include "queue_store.h"
+#include<iostream>
+#include <cstring>
+
+using namespace std;
+using namespace race2018;
+
+/**
+ * This in-memory implementation is for demonstration purpose only. You are supposed to modify it.
+ */
+void queue_store::put(const string& queue_name, const MemBlock& message) {
+    //lock_guard<mutex> lock(mtx);
+    
+    map_mtx.lock();
+    lock_guard<mutex> lock(mtx[queue_name]);
+    map_mtx.unlock();
+    
+    queue_map[queue_name].push_back(message);
+}
+
+/**
+ * This in-memory implementation is for demonstration purpose only. You are supposed to modify it.
+ */
+vector<MemBlock> queue_store::get(const std::string& queue_name, long offset, long number) {
+    //lock_guard<mutex> lock(mtx);
+
+    lock_guard<mutex> lock(mtx[queue_name]);
+
+    if (queue_map.find(queue_name) == queue_map.end()) {
+        return vector<MemBlock>();
+    }
+    auto queue = queue_map[queue_name];
+    if (offset >= queue.size()) {
+        return vector<MemBlock>();
+    }
+
+    vector<MemBlock> searchResult(queue.begin() + offset,
+                            offset + number > queue.size() ? queue.end() : queue.begin() + offset + number);
+
+    // Return deep copy of the MemBlock such that benchmark tool may release MemBlock::ptr safely.
+    vector<MemBlock> ret;
+    for (const auto &item : searchResult) {
+        MemBlock block;
+        block.size = item.size;
+        block.ptr = new char[block.size];
+        memcpy(block.ptr, item.ptr, block.size);
+        ret.push_back(block);
+    }
+
+    return ret;
+}
